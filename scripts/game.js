80b51703e93bd9c8f45fffa6968ce04cae1c0b57
kamilaalv <kamilaaliyeva58@gmail.com>
2022-10-12 00:15:04 +0300

    game more characters

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,47 @@
-charactersInfo = [
+
+
+  let charactersInfo = [
 	{
-	  name: "Xiao",
-	  image: "Xiao.png",
-	  element: "Anemo",
-	  weapon: "Polearm",
+	  name: "Albedo",
+	  image: "Albedo.png",
+	  element: "Geo",
+	  weapon: "Sword",
 	  star: 5,
+	},
+	{
+	  name: "Aloy",
+	  image: "Aloy.png",
+	  element: "Cryo",
+	  weapon: "Bow",
+	  star: 4,
+	},
+	{
+	  name: "Amber",
+	  image: "Amber.png",
+	  element: "Pyro",
+	  weapon: "Bow",
+	  star: 4,
+	},
+	{
+	  name: "Ayaka",
+	  image: "Ayaka.png",
+	  element: "Cryo",
+	  weapon: "Sword",
+	  star: 5,
+	},
+	{
+	  name: "Ayato",
+	  image: "Ayato.png",
+	  element: "Hydro",
+	  weapon: "Sword",
+	  star: 5,
+	},
+	{
+	  name: "Barbara",
+	  image: "Barbara.png",
+	  element: "Hydro",
+	  weapon: "Catalyst",
+	  star: 4,
 	},
 	{
 	  name: "Beidou",
@@ -14,12 +51,19 @@ charactersInfo = [
 	  star: 4,
 	},
 	{
-		name: "Rosaria",
-		image: "Rosaria.png",
-		element: "Cryo",
-		weapon: "Polearm",
-		star: 4,
-	  },
+	  name: "Bennett",
+	  image: "Bennett.png",
+	  element: "Pyro",
+	  weapon: "Sword",
+	  star: 4,
+	},
+	{
+	  name: "Candace",
+	  image: "Candace.png",
+	  element: "Hydro",
+	  weapon: "Polearm",
+	  star: 4,
+	},
 	{
 	  name: "Childe",
 	  image: "Childe.png",
@@ -28,32 +72,24 @@ charactersInfo = [
 	  star: 5,
 	},
 	{
-	  name: "Shenhe",
-	  image: "Shenhe.png",
+	  name: "Chongyun",
+	  image: "Chongyun.png",
 	  element: "Cryo",
-	  weapon: "Polearm",
-	  star: 5,
-	},
-	{
-	  name: "Sucrose",
-	  image: "Sucrose.png",
-	  element: "Anemo",
-	  weapon: "Catalyst",
+	  weapon: "Claymore",
 	  star: 4,
 	},
 	{
-	  name: "Yae Miko",
-	  image: "YaeMiko.png",
-	  element: "Electro",
-	  weapon: "Catalyst",
-	  star: 5,
-	},
-  
-	{
-	  name: "Yoimiya",
-	  image: "Yoimiya.png",
-	  element: "Pyro",
+	  name: "Collei",
+	  image: "Collei.png",
+	  element: "Dendro",
 	  weapon: "Bow",
+	  star: 4,
+	},
+	{
+	  name: "Cyno",
+	  image: "Cyno.png",
+	  element: "Electro",
+	  weapon: "Polearm",
 	  star: 5,
 	},
 	{
@@ -64,26 +100,26 @@ charactersInfo = [
 	  star: 5,
 	},
 	{
+	  name: "Diona",
+	  image: "Diona.png",
+	  element: "Cryo",
+	  weapon: "Bow",
+	  star: 4,
+	},
+	{
+	  name: "Dori",
+	  image: "Dori.png",
+	  element: "Electro",
+	  weapon: "Claymore",
+	  star: 4,
+	},
+	{
 	  name: "Eula",
 	  image: "Eula.png",
 	  element: "Cryo",
 	  weapon: "Claymore",
 	  star: 5,
 	},
-	{
-		name: "Venti",
-		image: "Venti.png",
-		element: "Anemo",
-		weapon: "Bow",
-		star: 5,
-	  },
-	{
-		name: "Yelan",
-		image: "Yelan.png",
-		element: "Hydro",
-		weapon: "Bow",
-		star: 5,
-	  },
 	{
 	  name: "Fischl",
 	  image: "Fischl.png",
@@ -99,6 +135,20 @@ charactersInfo = [
 	  star: 5,
 	},
 	{
+	  name: "Gorou",
+	  image: "Gorou.png",
+	  element: "Geo",
+	  weapon: "Bow",
+	  star: 4,
+	},
+	{
+	  name: "Heizou",
+	  image: "Heizou.png",
+	  element: "Anemo",
+	  weapon: "Catalyst",
+	  star: 4,
+	},
+	{
 	  name: "Hu Tao",
 	  image: "HuTao.png",
 	  element: "Pyro",
@@ -110,6 +160,13 @@ charactersInfo = [
 	  image: "Itto.png",
 	  element: "Geo",
 	  weapon: "Claymore",
+	  star: 5,
+	},
+	{
+	  name: "Jean",
+	  image: "Jean.png",
+	  element: "Anemo",
+	  weapon: "Sword",
 	  star: 5,
 	},
 	{
@@ -148,6 +205,13 @@ charactersInfo = [
 	  star: 5,
 	},
 	{
+	  name: "Kuki Shinobu",
+	  image: "Kuki.png",
+	  element: "Electro",
+	  weapon: "Sword",
+	  star: 4,
+	},
+	{
 	  name: "Lisa",
 	  image: "Lisa.png",
 	  element: "Electro",
@@ -162,18 +226,25 @@ charactersInfo = [
 	  star: 5,
 	},
 	{
+	  name: "Ningguang",
+	  image: "Ningguang.png",
+	  element: "Geo",
+	  weapon: "Catalyst",
+	  star: 4,
+	},
+	{
+	  name: "Noelle",
+	  image: "Noelle.png",
+	  element: "Geo",
+	  weapon: "Claymore",
+	  star: 4,
+	},
+	{
 	  name: "Qiqi",
 	  image: "Qiqi.png",
 	  element: "Cryo",
 	  weapon: "Sword",
 	  star: 5,
-	},
-	{
-	  name: "Gorou",
-	  image: "Gorou.png",
-	  element: "Geo",
-	  weapon: "Bow",
-	  star: 4,
 	},
 	{
 	  name: "Raiden",
@@ -183,32 +254,154 @@ charactersInfo = [
 	  star: 5,
 	},
 	{
+	  name: "Razor",
+	  image: "Razor.png",
+	  element: "Electro",
+	  weapon: "Claymore",
+	  star: 4,
+	},
+	{
+	  name: "Rosaria",
+	  image: "Rosaria.png",
+	  element: "Cryo",
+	  weapon: "Polearm",
+	  star: 4,
+	},
+	{
+	  name: "Sara",
+	  image: "Sara.png",
+	  element: "Electro",
+	  weapon: "Bow",
+	  star: 4,
+	},
+	{
+	  name: "Sayu",
+	  image: "Sayu.png",
+	  element: "Anemo",
+	  weapon: "Claymore",
+	  star: 4,
+	},
+	{
+	  name: "Shenhe",
+	  image: "Shenhe.png",
+	  element: "Cryo",
+	  weapon: "Polearm",
+	  star: 5,
+	},
+	{
+	  name: "Sucrose",
+	  image: "Sucrose.png",
+	  element: "Anemo",
+	  weapon: "Catalyst",
+	  star: 4,
+	},
+	{
+	  name: "Thoma",
+	  image: "Thoma.png",
+	  element: "Pyro",
+	  weapon: "Polearm",
+	  star: 4,
+	},
+	{
+	  name: "Tighnari",
+	  image: "Tighnari.png",
+	  element: "Dendro",
+	  weapon: "Bow",
+	  star: 5,
+	},
+	{
+	  name: "Venti",
+	  image: "Venti.png",
+	  element: "Anemo",
+	  weapon: "Bow",
+	  star: 5,
+	},
+	{
+	  name: "Xiangling",
+	  image: "Xiangling.png",
+	  element: "Pyro",
+	  weapon: "Polearm",
+	  star: 4,
+	},
+	{
+	  name: "Xiao",
+	  image: "Xiao.png",
+	  element: "Anemo",
+	  weapon: "Polearm",
+	  star: 5,
+	},
+	{
+	  name: "Xingqiu",
+	  image: "Xingqiu.png",
+	  element: "Hydro",
+	  weapon: "Sword",
+	  star: 4,
+	},
+	{
+	  name: "Xinyan",
+	  image: "Xinyan.png",
+	  element: "Pyro",
+	  weapon: "Claymore",
+	  star: 4,
+	},
+	{
+	  name: "Yae Miko",
+	  image: "YaeMiko.png",
+	  element: "Electro",
+	  weapon: "Catalyst",
+	  star: 5,
+	},
+	{
+	  name: "Yanfei",
+	  image: "Yanfei.png",
+	  element: "Pyro",
+	  weapon: "Catalyst",
+	  star: 4,
+	},
+	{
+	  name: "Yelan",
+	  image: "Yelan.png",
+	  element: "Hydro",
+	  weapon: "Bow",
+	  star: 5,
+	},
+	{
+	  name: "Yoimiya",
+	  image: "Yoimiya.png",
+	  element: "Pyro",
+	  weapon: "Bow",
+	  star: 5,
+	},
+	{
+	  name: "Yun Jin",
+	  image: "YunJin.png",
+	  element: "Geo",
+	  weapon: "Polearm",
+	  star: 4,
+	},
+	{
 	  name: "Zhongli",
 	  image: "Zhongli.png",
 	  element: "Geo",
 	  weapon: "Polearm",
 	  star: 5,
-	}, 
-	{
-	  name: "Heizou",
-	  image: "Heizou.png",
-	  element: "Anemo",
-	  weapon: "Catalyst",
-	  star: 5,
 	},
   ];
+
    /* MY PROJECT GAME */
   $(document).ready(function () {
 	/* Generate Prompt */
   
-	var elements = ["Pyro", "Hydro", "Electro", "Anemo", "Geo", "Cryo"];
+	var elements = ["Pyro", "Hydro", "Electro","Dendro", "Anemo", "Geo", "Cryo", ];
 	var elementImages = [
 	  "Element_Pyro.png",
 	  "Element_Hydro.png",
 	  "Element_Electro.png",
+	  "Element_Dendro.png",
 	  "Element_Anemo.png",
 	  "Element_Geo.png",
 	  "Element_Cryo.png",
+
 	];
 	
   
@@ -218,7 +411,7 @@ charactersInfo = [
 	while(chars.size !== 16) {
   		chars.add(Math.floor(Math.random() * (len - 1)) + 1);
 	}
-	let elementsCount = [0, 0, 0, 0, 0, 0];
+	let elementsCount = [0, 0, 0, 0, 0, 0, 0];
   
 	for (id of chars) {
 	  $(".grid-cont").append(
@@ -241,14 +434,17 @@ charactersInfo = [
 		case "Electro":
 		  elementsCount[2]++;
 		  break;
-		case "Anemo":
+		case "Dendro":
 		  elementsCount[3]++;
 		  break;
-		case "Geo":
+		case "Anemo":
 		  elementsCount[4]++;
 		  break;
-		case "Cryo":
+		case "Geo":
 		  elementsCount[5]++;
+		  break;
+		case "Cryo":
+		  elementsCount[6]++;
 		  break;
 	  }
 	}
@@ -304,14 +500,12 @@ charactersInfo = [
 		
 	  }
 	  if (score == 160){
-		  $("h2").remove()
-		  $(".grid-cont").html("<h1> WIN!!! </h1>")
+		  $("h2").remove();
+		  $("img").remove();
+		  $(".grid-cont").html("<h1> WIN!!! </h1>");
 		  $(".grid-cont").css({"height":"400px",'background-image':'url("Images/confetti-10.gif")'});
-  
+			//add replay
 	  }
 	});
-	
-	
-  
   });
   
